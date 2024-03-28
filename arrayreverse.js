0e0993fff100e6f328184d2d449d73ec37d9ebf2
Guillaume <guillaumebaillieux88@gmail.com>
2024-03-28 15:52:49 +0100

    firscommit

---
+++ arrayreverse.js
@@ -0,0 +1,12 @@
+
+
+function reverseArray(arrayToReverse) {
+   let arrayReversed = [...arrayToReverse];
+
+   while (array.length > 0){
+   arrayReversed.push( array.pop());
+   }
+   return arrayReversed;
+}
+
+reverseArray(myArray);
