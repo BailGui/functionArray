36d4ad1ebb844094042d794d605c0d64d6490ced
Guillaume <guillaumebaillieux88@gmail.com>
2024-04-10 16:08:55 +0200

    maj

--- exercices.js
+++ exercices.js
@@ -12,3 +12,27 @@ arrayMots = ["Rien", "ne", "sert", "de", "courir", ";", "il", "faut", "partir", 
     }
     
  }
+
+
+ /* A partir de l'array suivant [9, 45, 90, 37, 36, 54] donner l'index de la première valeur qui n'est pas entièrement divisible par 3*/
+
+ var myArray = [9, 45, 90, 37, 36, 54]
+
+ let myNewArray = myArray.findIndex(e => {return e%3}); 
+
+ /* A partir de l'arrêt suivant ["Rami","Emrah","Rebecca","Joel","Frank","Lee"] filtrer les noms pour que seulement les motes commençant par 'R' restent*/
+
+ var myArray = ["Rami","Emrah","Rebecca","Joel","Frank","Lee"]
+
+ let myNewArray2 = myArray.filter(e=> {return e[0] === "R"});
+
+ /*En utilisant la méthode d'array appropriée, classez les chiffres ci-dessous par ordre "alphabétique" 
+ [17, 1, 33, 111, 27, 3, 27, 22, 1323]*/
+
+ var myArray = [17, 1, 33, 111, 27, 3, 27, 22, 1323];
+ myArray.sort();
+ console.log(myArray);
+
+
+ 
+ 
